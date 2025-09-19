6e83d3b8dc63e9101c78b8ae02f8ff6c37506a3b
AjinkyaGude <ajinkyagude@gmail.com>
2025-09-19 12:48:29 +0530

    Major Update sept 2025
    Major Update sept 2025

--- Source/script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".ham-menu-links");
-    const icon = document.querySelector(".ham-icon");
-
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
